de6fce39e7a2d169330929f905467d6acb890168
dhavalusdad <dhaval.usdad@esparkbizmail.com>
2025-05-21 15:38:50 +0530

    (CM) : demographics add

--- src/components/Layout/Forms/Demographics.tsx
+++ src/components/Layout/Forms/Demographics.tsx
@@ -5,7 +5,6 @@ import SelectDatePicker from '@encounter/common/components/Datepicker'; // Ensur
 export const Demographics = () => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   return (
-<<<<<<< HEAD
     <div className="space-y-6">
       <h1 className="text-xl font-semibold text-Primary-900">Demographics / Intake</h1>
 
@@ -82,10 +81,5 @@ export const Demographics = () => {
     </div>
   );
 };
-=======
-    <div>Demographics</div>
-  )
-}
->>>>>>> 716e3b7ac90092412560d73bd4f7d5718265ab94
 
 export default Demographics;
